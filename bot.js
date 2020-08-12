ddce6a6984a977730c5e1ca103c303beb6912431
mahdifazzi <69550902+mahdifazzi@users.noreply.github.com>
2020-08-12 09:24:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
-const myid = ['Your ID']
-const client = new Discord.Client();
-const client2 = new Discord.Client();
+const myid = ['583718628554309644']
+const client = 742907686701760512.Client();
+const client2 =742848071330955286.Client();
 const client3 = new Discord.Client();
 const client4 = new Discord.Client();
 const client5 = new Discord.Client();
@@ -27,8 +27,8 @@ const client24 = new Discord.Client();
 
 
 
-client.login(process.env.TOKEN);
-client2.login(process.env.TOKEN2);
+client.login(NzQyODQ1MDkyMTM3NjY0NjY0XzM3Xgl0b8fLStI7CJJLinBHfNLhGjr0Q.env.TOKEN);
+client2.login(NzQyOTA3Njg2NzAxNzYwNTEyXzM9YwIUi1eqrVFxq4AJ1EEsRwY7PaGxM.env.TOKEN2);
 client3.login(process.env.TOKEN3);
 client4.login(process.env.TOKEN4);
 client5.login(process.env.TOKEN5);
