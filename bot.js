0bd3437f09b1fa1f4640f3589d4492b8187018ea
mahdifazzi <69550902+mahdifazzi@users.noreply.github.com>
2020-08-12 12:23:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -387,7 +387,7 @@ if (message.content === 'go') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 25000; x++) {
-        message.channel.send(`benjeedou`)
+        message.channel.send(`hi`)
           .then(m => {
             count++;
           })
@@ -401,7 +401,7 @@ if (message.content === 'go') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 25000; x++) {
-        message.channel.send(`benjeedou`)
+        message.channel.send(`hi`)
           .then(m => {
             count++;
           })
@@ -415,7 +415,7 @@ if (message.content === 'go') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 25000; x++) {
-        message.channel.send(`benjeedou`)
+        message.channel.send(`hi`)
           .then(m => {
             count++;
           })
@@ -429,7 +429,7 @@ if (message.content === 'go') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 25000; x++) {
-        message.channel.send(`benjeedou`)
+        message.channel.send(`hi`)
           .then(m => {
             count++;
           })
@@ -443,7 +443,7 @@ if (message.content === 'go') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 9000; x++) {
-        message.channel.send(`benjeedou`)
+        message.channel.send(`hi`)
           .then(m => {
             count++;
           })
