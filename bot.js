7cf6ad72375309fddad33fe1eb9610851fec9645
mahdifazzi <69550902+mahdifazzi@users.noreply.github.com>
2020-08-12 12:21:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -373,7 +373,7 @@ if (message.content === 'spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 25000; x++) {
-        message.channel.send(`benjeedou`)
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
